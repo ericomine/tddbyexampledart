ab9cd48ff820b6c3b7a88059f6c333f85df701a6
Eric Omine <5587665-ericomine@users.noreply.gitlab.com>
2020-05-05 23:12:51 -0300

    first test

--- test/dollar_test.dart
+++ test/dollar_test.dart
@@ -2,8 +2,10 @@ import 'package:test/test.dart';
 
 void main() {
 
-  test('', () {
-    
+  test('should multiply', () {
+    Dollar five = Dollar(5);
+    five.times(2);
+    expect(10, five.ammount);
   });
 
 }
