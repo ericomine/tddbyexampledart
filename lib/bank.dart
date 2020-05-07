9052f397565cf38bbd060058ad5da311cdc1ab64
Eric Omine <eric.omine@gmail.com>
2020-05-07 20:24:31 -0300

    Added tuple dep, used in bank to implement add rates

--- lib/bank.dart
+++ lib/bank.dart
@@ -1,12 +1,18 @@
+import 'dart:collection';
+
 import 'package:tddbyexample_dart/expression.dart';
 import 'package:tddbyexample_dart/money.dart';
+import 'package:tuple/tuple.dart';
 
 class Bank {
+  var rates = HashMap<Tuple2<String, String>, num>();
+
   Money reduce(Expression source, String to) {
     return source.reduce(this, to);
   }
 
   void addRate(String fromCurrency, String toCurrency, num rate) {
-    return null;
+    var key = Tuple2<String, String>(fromCurrency, toCurrency);
+    rates[key] = rate;
   }
 }
