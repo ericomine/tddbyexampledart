b92a40f6af71f9a283d538ab1b8b71b9b2ee32e2
Eric Omine <5587665-ericomine@users.noreply.gitlab.com>
2020-05-08 07:25:38 -0300

    Initial tests for part2

---
+++ test/part2/part2_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('Invoke test()', () {});
+  test('Invoke setUp() first', () {});
+  test('Invoke tearDown() later', () {});
+  test('Invoke tearDown() even if test fails', () {});
+  test('Invoke multiple tests', () {});
+  test('Report test results', () {});
+}
