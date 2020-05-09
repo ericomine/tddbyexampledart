d8124ed422f838a0d876dabc41a24b39af9eb558
Eric Omine <5587665-ericomine@users.noreply.gitlab.com>
2020-05-09 19:20:28 -0300

    First test for part 2

--- test/part2/part2_test.dart
+++ test/part2/part2_test.dart
@@ -1,7 +1,14 @@
+import 'package:tddbyexample_dart/part2/wasrun.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Invoke test()', () {});
+  test('Invoke test()', () {
+    var myTest = WasRun("testMethod");
+    print(myTest.wasrun);
+    myTest.testMethod();
+    print(myTest.wasrun);
+
+  });
   test('Invoke setUp() first', () {});
   test('Invoke tearDown() later', () {});
   test('Invoke tearDown() even if test fails', () {});
